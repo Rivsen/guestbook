f310d40f613d4772bdd37500c889940970188d35
Saffi Bai <sufang.bai@gmail.com>
2012-12-29 19:09:54 +0800

    app/console doctrine:generate:crud
    RsworkGuestbookBundle:Message
    generate write: yes
    config format: yml
    prefix: /guestbook

---
+++ src/Rswork/GuestbookBundle/Controller/MessageController.php
@@ -0,0 +1,179 @@
+<?php
+
+namespace Rswork\GuestbookBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Rswork\GuestbookBundle\Entity\Message;
+use Rswork\GuestbookBundle\Form\MessageType;
+
+/**
+ * Message controller.
+ *
+ */
+class MessageController extends Controller
+{
+    /**
+     * Lists all Message entities.
+     *
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('RsworkGuestbookBundle:Message')->findAll();
+
+        return $this->render('RsworkGuestbookBundle:Message:index.html.twig', array(
+            'entities' => $entities,
+        ));
+    }
+
+    /**
+     * Finds and displays a Message entity.
+     *
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('RsworkGuestbookBundle:Message')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Message entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+
+        return $this->render('RsworkGuestbookBundle:Message:show.html.twig', array(
+            'entity'      => $entity,
+            'delete_form' => $deleteForm->createView(),        ));
+    }
+
+    /**
+     * Displays a form to create a new Message entity.
+     *
+     */
+    public function newAction()
+    {
+        $entity = new Message();
+        $form   = $this->createForm(new MessageType(), $entity);
+
+        return $this->render('RsworkGuestbookBundle:Message:new.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        ));
+    }
+
+    /**
+     * Creates a new Message entity.
+     *
+     */
+    public function createAction(Request $request)
+    {
+        $entity  = new Message();
+        $form = $this->createForm(new MessageType(), $entity);
+        $form->bind($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('guestbook_show', array('id' => $entity->getId())));
+        }
+
+        return $this->render('RsworkGuestbookBundle:Message:new.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing Message entity.
+     *
+     */
+    public function editAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('RsworkGuestbookBundle:Message')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Message entity.');
+        }
+
+        $editForm = $this->createForm(new MessageType(), $entity);
+        $deleteForm = $this->createDeleteForm($id);
+
+        return $this->render('RsworkGuestbookBundle:Message:edit.html.twig', array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Edits an existing Message entity.
+     *
+     */
+    public function updateAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('RsworkGuestbookBundle:Message')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Message entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+        $editForm = $this->createForm(new MessageType(), $entity);
+        $editForm->bind($request);
+
+        if ($editForm->isValid()) {
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('guestbook_edit', array('id' => $id)));
+        }
+
+        return $this->render('RsworkGuestbookBundle:Message:edit.html.twig', array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a Message entity.
+     *
+     */
+    public function deleteAction(Request $request, $id)
+    {
+        $form = $this->createDeleteForm($id);
+        $form->bind($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $entity = $em->getRepository('RsworkGuestbookBundle:Message')->find($id);
+
+            if (!$entity) {
+                throw $this->createNotFoundException('Unable to find Message entity.');
+            }
+
+            $em->remove($entity);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('guestbook'));
+    }
+
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder(array('id' => $id))
+            ->add('id', 'hidden')
+            ->getForm()
+        ;
+    }
+}
---
+++ src/Rswork/GuestbookBundle/Form/MessageType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Rswork\GuestbookBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class MessageType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('author')
+            ->add('published')
+            ->add('content')
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Rswork\GuestbookBundle\Entity\Message'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'rswork_guestbookbundle_messagetype';
+    }
+}
